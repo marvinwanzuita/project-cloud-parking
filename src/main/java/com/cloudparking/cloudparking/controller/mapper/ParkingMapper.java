3a14a4d4246708fd4ec960cfc3247163701ab32f
Marvin <marvinwanzuita@gmail.com>
2022-08-05 16:35:21 -0300

    first list endpoint

---
+++ src/main/java/com/cloudparking/cloudparking/controller/mapper/ParkingMapper.java
@@ -0,0 +1,24 @@
+package com.cloudparking.cloudparking.controller.mapper;
+
+import com.cloudparking.cloudparking.dto.ParkingDTO;
+import com.cloudparking.cloudparking.model.Parking;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ParkingMapper {
+
+    private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+    public ParkingDTO parkingDTO(Parking parking){
+        return MODEL_MAPPER.map(parking, ParkingDTO.class);
+    }
+
+    public List<ParkingDTO> toParkingDTOList(List<Parking> parkingList){
+        return parkingList.stream().map(this::parkingDTO).collect(Collectors.toList());
+    }
+
+}
