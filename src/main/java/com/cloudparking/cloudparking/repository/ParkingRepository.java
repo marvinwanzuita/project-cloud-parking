38ceb6d12f27e29d5f89e028c90edaf90b4ccb23
Marvin <marvinwanzuita@gmail.com>
2022-08-07 21:30:53 -0300

    Swagger

---
+++ src/main/java/com/cloudparking/cloudparking/repository/ParkingRepository.java
@@ -0,0 +1,13 @@
+package com.cloudparking.cloudparking.repository;
+
+
+import com.cloudparking.cloudparking.model.Parking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<Parking, String> {
+
+
+
+}
--- src/main/java/com/cloudparking/cloudparking/service/ParkingService.java
+++ src/main/java/com/cloudparking/cloudparking/service/ParkingService.java
@@ -2,22 +2,25 @@ package com.cloudparking.cloudparking.service;
 
 import com.cloudparking.cloudparking.exception.ParkingNotFoundException;
 import com.cloudparking.cloudparking.model.Parking;
+import com.cloudparking.cloudparking.repository.ParkingRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 public class ParkingService {
 
-    private static Map<String, Parking> parkingMap = new HashMap<>();
+    private final ParkingRepository parkingRepository;
+
+    public ParkingService(ParkingRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
+    }
 
     public List<Parking> findAll(){
-        return parkingMap.values().stream().collect(Collectors.toList());
+         return parkingRepository.findAll();
     }
 
     private static String getUUID(){
@@ -25,30 +28,30 @@ public class ParkingService {
     }
 
     public Parking findById(String id) {
-        Parking parking =parkingMap.get(id);
-        if (parking == null){
-            throw new ParkingNotFoundException(id);
-        }
-        return parking;
+        return parkingRepository.findById(id).orElseThrow(() ->
+                new ParkingNotFoundException(id));
     }
 
     public Parking create(Parking parkingCreate){
         String uuid = getUUID();
         parkingCreate.setId(uuid);
         parkingCreate.setEntryDate(LocalDateTime.now());
-        parkingMap.put(uuid, parkingCreate);
+        parkingRepository.save(parkingCreate);
         return parkingCreate;
     }
 
     public void delete(String id) {
         findById(id);
-        parkingMap.remove(id);
+        parkingRepository.deleteById(id);
     }
 
     public Parking update(String id, Parking parkingCreate) {
         Parking parking = findById(id);
         parking.setColor(parkingCreate.getColor());
-        parkingMap.replace(id, parking);
+        parking.setState(parkingCreate.getState());
+        parking.setModel(parkingCreate.getModel());
+        parking.setLicense(parkingCreate.getLicense());
+        parkingRepository.save(parking);
         return parking;
     }
 
